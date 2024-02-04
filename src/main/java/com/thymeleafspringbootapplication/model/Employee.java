84d2329be84aaed1e8aee03a536f19a8b10055fe
Vaibhavi Khaire <vaibhavikhire81@gmail.com>
2024-02-04 23:22:07 +0530

    Done

--- src/main/java/com/thymeleafspringbootapplication/model/Employee.java
+++ src/main/java/com/thymeleafspringbootapplication/model/Employee.java
@@ -24,37 +24,76 @@ public class Employee {
 	
 	@Column(name = "email")
 	private String email;
-
+	
+	@Column(name= "gender")
+	private String gender;
+	
+	@Column(name= "position")
+	private String position;
+	
+	@Column(name="salary")
+	private String salary;
+	
+	@Column(name= "contact_no")
+	private String contact_no;
+	@Column(name="address")
+	private String address;
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(String contact_no) {
+		this.contact_no = contact_no;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+
 	
 }
